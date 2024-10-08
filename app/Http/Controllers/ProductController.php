d7624585533550c895bb62ffebd824ce711c3fe2
prandika119 <an.prasetya9@gmail.com>
2024-10-08 14:56:03 +0700

    commit pertama

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
-class Controller extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index() : View
+    public function index(): View
     {
-        return view('index', [
+        return view('products.index', [
             'products' => Product::latest()->paginate(3)
         ]);
     }
@@ -22,7 +25,7 @@ class Controller extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create() : View
+    public function create(): View
     {
         return view('products.create');
     }
@@ -30,27 +33,27 @@ class Controller extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProductRequest $request) : RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
         Product::create($request->all());
-        return redirect()->route('index')
-                ->withSuccess('New product is added successfully.');
+        return redirect()->route('products.index')
+            ->withSuccess('New product is added successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product) : View
+    public function show(Product $product): View
     {
         return view('products.show', [
-            'product' => $products
+            'product' => $product
         ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product) : View
+    public function edit(Product $product): View
     {
         return view('products.edit', [
             'products' => $product
@@ -60,20 +63,20 @@ class Controller extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product) : RedirectResponse
+    public function update(UpdateProductRequest $request, Product $product): RedirectResponse
     {
         $product->update($request->all());
         return redirect()->back()
-                ->withSuccess('Product is updated successfully.');
+            ->withSuccess('Product is updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product) : RedirectResponse
+    public function destroy(Product $product): RedirectResponse
     {
         $product->delete();
-        return redirect()->route('index')
-                ->withSuccess('Product is deleted successfully.');
+        return redirect()->route('products.index')
+            ->withSuccess('Product is deleted successfully.');
     }
 }
